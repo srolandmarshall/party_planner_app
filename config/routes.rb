8a582c6f49c997e6da16638783d248468f1cbd1d
Sam Marshall <samuel.r.marshall@gmail.com>
2018-02-12 20:22:50 -0500

    added foods and drinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :drinks
+  resources :foods
   devise_for :users
   resources :parties
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213011941) do
+ActiveRecord::Schema.define(version: 20180213012215) do
+
+  create_table "drinks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parties", force: :cascade do |t|
     t.string   "name"
