1902b7f2b53a4e643918198850a8251a452efd7d
srolandmarshall <samuel.r.marshall@gmail.com>
2018-02-13 16:58:05 -0500

    removed unnecessary join table

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,11 +3,15 @@ class Party < ActiveRecord::Base
   has_many :foods
   has_many :drinks
   belongs_to :host, class_name: 'User'
-  has_and_belongs_to_many :attendees, class_name: 'User' 
+  has_and_belongs_to_many :attendees, class_name: 'User'
 
 
   def display_time
     self.time.strftime("%A, %D, %H:%M%P")
+  end
+
+  def attend(user)
+    self.attendees << user
   end
 
 end
--- db/migrate/20180213194250_create_parties_users.rb
+++
@@ -1,8 +0,0 @@
-class CreatePartiesUsers < ActiveRecord::Migration
-  def change
-    create_table :parties_users do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
